4b91c856eb1ed1b19c718190c8fad6439a3088f3
Sairaj Patil <sairajgpatil8@gmail.com>
2026-01-13 12:45:32 +0530

    feat: add protected routes and role-based access control

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import AdminZones from "./pages/AdminZones";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Protected Home (all logged-in users) */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Auth Pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* Admin-only Route */}
+        <Route
+          path="/admin/zones"
+          element={
+            <ProtectedRoute roles={["admin"]}>
+              <AdminZones />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
